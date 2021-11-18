17cf4684598d6387e774eb21e23fb0040ee60cdf
liggia <liggia.cruz@gmail.com>
2021-11-18 17:30:32 -0300

    feat: initial settings

---
+++ src/setup.js
@@ -0,0 +1,18 @@
+import dotenv from 'dotenv';
+
+let path;
+
+switch (process.env.NODE_ENV) {
+    case 'prod':
+      path = '.env.prod';
+      break;
+    case 'test':
+      path = '.env.test';
+      break;
+    default:
+      path = '.env.dev';
+  }
+
+dotenv.config({
+    path,
+});
